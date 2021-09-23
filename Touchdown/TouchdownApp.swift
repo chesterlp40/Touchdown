c93ec5b080d3a0d8f6c2c0c883ba1a3f82879100
Ezequiel Rasgido <ezequiel.rasgido@mercadolibre.com>
2021-09-23 19:47:25 -0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Ezequiel Rasgido on 23/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
